90f5c31db18a979b68c46caddcb666f3b25f68c7
Sherry <152143173+SerryHuang@users.noreply.github.com>
2023-12-10 00:59:13 +0800

    Update main.c

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ int main(void)
 	int board[BOARD][BOARD];
 	int virtual[BOARD][BOARD] = { {1,2,3,4,5},{6,7,8,9,10},
 		{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25} };
-	int PLYINPUT, COMINPUT;
+	int PLYINPUT, COMINPUT, plycheck;
 	char changePlayer = PLAYER;
 	originalboard(board);
 	do
@@ -18,14 +18,22 @@ int main(void)
 			break;
 		else if (changePlayer == PLAYER)
 		{
-			printf("ª±®a %c ½Ğ¿é¤J¼Æ¦r(1-25): ", changePlayer);
+			printf("ÂªÂ±Â®a %c Â½ÃÂ¿Ã©Â¤JÂ¼Ã†Â¦r(1-25): ", changePlayer);
 			scanf("%d", &PLYINPUT);
-			playerenter(virtual, board, PLYINPUT);
+			plycheck = playerenter(virtual, board, PLYINPUT);
+			while (plycheck == 1)
+			{
+				printf("è¼¸å…¥éŒ¯èª¤ï¼Œæˆ–è©²ä½å€å·²è¢«ä½”æ“šï¼Œè«‹é‡æ–°è¼¸å…¥æ•¸å­—: ");
+				scanf("%d", &PLYINPUT);
+				plycheck = playerenter(PLYINPUT, virtual, board);
+			}
+			correct(board, &check);
 		}
 		else
 		{
-			printf("ª±®a %c ¿é¤J: \n", changePlayer);
+			printf("ÂªÂ±Â®a %c Â¿Ã©Â¤J: \n", changePlayer);
 			computerenter(virtual, board);
+			correct(board, &check);
 		}
 		changePlayer = (changePlayer == PLAYER) ? COMPUTER : PLAYER;
 	} while (check == 0);
@@ -33,18 +41,30 @@ int main(void)
 	return 0;
 }
 
-void playerenter(int virtual[][BOARD], int board[][BOARD], int PLYINPUT)
+int playerenter(int PLYINPUT, int virtual[][BOARD], int board[][BOARD])
 {
-	virtual[BOARD][BOARD];
-	for (int i = 0;i < BOARD;i++)
+	if (PLYINPUT < 1 || PLYINPUT>25)
+		return 1;
+	else
 	{
-		for (int j = 0;j < BOARD;j++)
+		virtual[BOARD][BOARD];
+		for (int i = 0;i < BOARD;i++)
 		{
-			if (virtual[i][j] == PLYINPUT)
-				board[i][j] = PLAYER;
+			for (int j = 0;j < BOARD;j++)
+			{
+				if (virtual[i][j] == PLYINPUT)
+				{
+					if (board[i][j] != EMPTY)
+						return 1;
+					else
+					{
+						board[i][j] = PLAYER;
+						return 0;
+					}
+				}
+			}
 		}
 	}
-	correct(board, &check);
 }
 
 void computerenter(int virtual[][BOARD], int board[][BOARD])
@@ -68,7 +88,6 @@ void computerenter(int virtual[][BOARD], int board[][BOARD])
 		}
 	} while (flag == 0);
 	printfboard(board);
-	correct(board, &check);
 }
 
 void correct(int board[][BOARD], int* check)
@@ -77,16 +96,16 @@ void correct(int board[][BOARD], int* check)
 	{
 		if (board[i][0] == PLAYER && board[i][1] == PLAYER && board[i][2] == PLAYER && board[i][3] == PLAYER && board[i][4] == PLAYER)
 		{
-			printf("%c ª±®aÀò³Ó!!\n", PLAYER);
+			printf("%c ÂªÂ±Â®aÃ€Ã²Â³Ã“!!\n", PLAYER);
 			printfboard(board);
-			printf("¹CÀ¸µ²§ô\n");
+			printf("Â¹CÃ€Â¸ÂµÂ²Â§Ã´\n");
 			*check = 1;
 		}
 		if (board[i][0] == COMPUTER && board[i][1] == COMPUTER && board[i][2] == COMPUTER && board[i][3] == COMPUTER && board[i][4] == COMPUTER)
 		{
-			printf("%c ª±®aÀò³Ó!!\n", COMPUTER);
+			printf("%c ÂªÂ±Â®aÃ€Ã²Â³Ã“!!\n", COMPUTER);
 			printfboard(board);
-			printf("¹CÀ¸µ²§ô\n");
+			printf("Â¹CÃ€Â¸ÂµÂ²Â§Ã´\n");
 			*check = 1;
 		}
 	}
@@ -94,45 +113,45 @@ void correct(int board[][BOARD], int* check)
 	{
 		if (board[0][i] == PLAYER && board[1][i] == PLAYER && board[2][i] == PLAYER && board[3][i] == PLAYER && board[4][i] == PLAYER)
 		{
-			printf("%c ª±®aÀò³Ó!!\n", PLAYER);
+			printf("%c ÂªÂ±Â®aÃ€Ã²Â³Ã“!!\n", PLAYER);
 			printfboard(board);
-			printf("¹CÀ¸µ²§ô\n");
+			printf("Â¹CÃ€Â¸ÂµÂ²Â§Ã´\n");
 			*check = 1;
 		}
 		else if (board[0][i] == COMPUTER && board[1][i] == COMPUTER && board[2][i] == COMPUTER && board[3][i] == COMPUTER && board[4][i] == COMPUTER)
 		{
-			printf("%c ª±®aÀò³Ó!!\n", COMPUTER);
+			printf("%c ÂªÂ±Â®aÃ€Ã²Â³Ã“!!\n", COMPUTER);
 			printfboard(board);
-			printf("¹CÀ¸µ²§ô\n");
+			printf("Â¹CÃ€Â¸ÂµÂ²Â§Ã´\n");
 			*check = 1;
 		}
 	}
 	if (board[0][0] == PLAYER && board[1][1] == PLAYER && board[2][2] == PLAYER && board[3][3] == PLAYER && board[4][4] == PLAYER)
 	{
-		printf("%c ª±®aÀò³Ó!!\n", PLAYER);
+		printf("%c ÂªÂ±Â®aÃ€Ã²Â³Ã“!!\n", PLAYER);
 		printfboard(board);
-		printf("¹CÀ¸µ²§ô\n");
+		printf("Â¹CÃ€Â¸ÂµÂ²Â§Ã´\n");
 		*check = 1;
 	}
 	else if (board[0][4] == PLAYER && board[1][3] == PLAYER && board[2][2] == PLAYER && board[3][1] == PLAYER && board[4][0] == PLAYER)
 	{
-		printf("%c ª±®aÀò³Ó!!\n", PLAYER);
+		printf("%c ÂªÂ±Â®aÃ€Ã²Â³Ã“!!\n", PLAYER);
 		printfboard(board);
-		printf("¹CÀ¸µ²§ô\n");
+		printf("Â¹CÃ€Â¸ÂµÂ²Â§Ã´\n");
 		*check = 1;
 	}
 	else if (board[0][0] == COMPUTER && board[1][1] == COMPUTER && board[2][2] == COMPUTER && board[3][3] == COMPUTER && board[4][4] == COMPUTER)
 	{
-		printf("%c ª±®aÀò³Ó!!\n", COMPUTER);
+		printf("%c ÂªÂ±Â®aÃ€Ã²Â³Ã“!!\n", COMPUTER);
 		printfboard(board);
-		printf("¹CÀ¸µ²§ô\n");
+		printf("Â¹CÃ€Â¸ÂµÂ²Â§Ã´\n");
 		*check = 1;
 	}
 	else if (board[0][4] == COMPUTER && board[1][3] == COMPUTER && board[2][2] == COMPUTER && board[3][1] == COMPUTER && board[4][0] == COMPUTER)
 	{
-		printf("%c ª±®aÀò³Ó!!\n", COMPUTER);
+		printf("%c ÂªÂ±Â®aÃ€Ã²Â³Ã“!!\n", COMPUTER);
 		printfboard(board);
-		printf("¹CÀ¸µ²§ô\n");
+		printf("Â¹CÃ€Â¸ÂµÂ²Â§Ã´\n");
 		*check = 1;
 	}
 }
